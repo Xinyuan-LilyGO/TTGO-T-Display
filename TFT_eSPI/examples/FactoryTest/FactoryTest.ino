5ccb17d15b5ad2308fd274e9efecb7c98058d384
lewisxhe <lewishe@outlook.com>
2021-01-22 13:44:00 +0800

    Fix wrong parameters

--- TFT_eSPI/examples/FactoryTest/FactoryTest.ino
+++ TFT_eSPI/examples/FactoryTest/FactoryTest.ino
@@ -209,8 +209,7 @@ void setup()
     button_init();
 
     esp_adc_cal_characteristics_t adc_chars;
-    esp_adc_cal_value_t val_type = esp_adc_cal_characterize((adc_unit_t)ADC_UNIT_1, (adc_atten_t)ADC1_CHANNEL_6, (adc_bits_width_t)ADC_WIDTH_BIT_12, 1100, &adc_chars);
-    //Check type of calibration value used to characterize ADC
+    esp_adc_cal_value_t val_type = esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, &adc_chars);    //Check type of calibration value used to characterize ADC
     if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) {
         Serial.printf("eFuse Vref:%u mV", adc_chars.vref);
         vref = adc_chars.vref;
